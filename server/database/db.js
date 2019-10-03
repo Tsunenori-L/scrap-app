967efa43bc69f7b2865dff499a5bde156d0a1535
Benson <ben72s5on@gmail.com>
2019-10-03 01:01:09 -0400

    added heroku db

--- server/database/db.js
+++ server/database/db.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/keepsake',
+  process.env.HEROKU_POSTGRESQL_AMBER_URL ||
+    'postgres://localhost:5432/keepsake',
   {
     logging: false,
-  },
+  }
 );
 
 // On CLI, enter : createdb keepsake
